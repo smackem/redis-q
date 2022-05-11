25934dfc53b7091f402d5f66711f29155ac6f261
smackem <sm4ckem@outlook.com>
2022-05-11 22:03:33 +0200

    adjusted option names to conform to docker/dotnet

--- src/RedisQ.Cli/Options.cs
+++ src/RedisQ.Cli/Options.cs
@@ -9,7 +9,7 @@ namespace RedisQ.Cli;
 // ReSharper disable once ClassNeverInstantiated.Global
 public record Options
 {
-    [Value(0, MetaName = "connectionString", Default = "localhost:6379", HelpText = "Redis connection string")]
+    [Value(0, MetaName = "connection-string", Default = "localhost:6379", HelpText = "Redis connection string")]
     public string ConnectionString { get; set; } = "localhost:6379";
 
     [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
@@ -18,9 +18,9 @@ public record Options
     [Option('s', "simple", Required = false, Default = false, HelpText = "Use a simplistic REPL without syntax highlighting.")]
     public bool Simple { get; set; }
 
-    [Option("chunksize", Required = false, Default = 100, HelpText = "The number of rows to print per chunk when displaying lists")]
+    [Option("chunk-size", Required = false, Default = 100, HelpText = "The number of rows to print per chunk when displaying lists")]
     public int ChunkSize { get; set; } = 100;
 
-    [Option('c', "caseSensitive", Required = false, Default = false, HelpText = "Function lookup is case-sensitive")]
+    [Option('c', "case-sensitive", Required = false, Default = false, HelpText = "Function lookup is case-sensitive")]
     public bool IsCaseSensitive { get; set; }
 }
