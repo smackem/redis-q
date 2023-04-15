e4ea799f0001d09fd39ab60efd23dec443683780
smackem <sm4ckem@outlook.com>
2023-04-15 14:05:47 +0200

    fixed #98

--- src/RedisQ.Cli/CustomFunctions.cs
+++ src/RedisQ.Cli/CustomFunctions.cs
@@ -1,4 +1,5 @@
-﻿using RedisQ.Core.Runtime;
+﻿using RedisQ.Core;
+using RedisQ.Core.Runtime;
 
 namespace RedisQ.Cli;
 
@@ -7,8 +8,19 @@ internal static class CustomFunctions
     public static void Bind(Context ctx)
     {
         const string dirIdent = "dir";
-        var dirFunction = new FunctionValue(dirIdent, Array.Empty<string>(),
-            new FunctionInvocationExpr("SCAN", new[] {new LiteralExpr(new StringValue("*"))}));
-        ctx.Bind(dirIdent, dirFunction);
+        ctx.Bind(dirIdent, CreateFunctionDir(dirIdent));
+
+        const string lsIdent = "ls";
+        ctx.Bind(lsIdent, CreateFunctionLs(lsIdent));
     }
+
+    private static FunctionValue CreateFunctionDir(string ident) =>
+        new(ident, Array.Empty<string>(),
+            new FunctionInvocationExpr("SCAN", new[] {new LiteralExpr(new StringValue("*"))}));
+
+    private static FunctionValue CreateFunctionLs(string ident) =>
+        new(ident, new[] {"sub"},
+            new Compiler().Compile(@"
+let pat = from c in chars(sub) select '[' + lower(c) + upper(c) + ']' |> join('') in
+scan('*' + pat + '*');"));
 }
