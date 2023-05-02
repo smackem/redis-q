ddbdfa051dc02c477fec0276bf7c5b2ab849ce32
smackem <sm4ckem@outlook.com>
2023-05-02 19:53:12 +0200

    resolved #102 (#109)

---
+++ tests/RedisQ.Core.Test/PipelineTests.cs
@@ -0,0 +1,25 @@
+﻿using RedisQ.Core.Runtime;
+using Xunit;
+
+namespace RedisQ.Core.Test;
+
+public class PipelineTests : TestBase
+{
+    [Fact]
+    public async Task PipelineWithRhsExpr()
+    {
+        var ctx = Context.Root(Helpers.DummyRedis, Helpers.DefaultFunctions);
+        var expr = Compile(@"1 |> { 2 + $ }");
+        var value = await expr.Evaluate(ctx);
+        Assert.Equal(IntegerValue.Of(3), value);
+    }
+
+    [Fact]
+    public async Task MultiPipelineWithRhsExpr()
+    {
+        var ctx = Context.Root(Helpers.DummyRedis, Helpers.DefaultFunctions);
+        var expr = Compile(@"1 |> { 2 + $ } |> { 3 + $ }");
+        var value = await expr.Evaluate(ctx);
+        Assert.Equal(IntegerValue.Of(6), value);
+    }
+}
